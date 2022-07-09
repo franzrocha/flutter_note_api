f9cce1d78394d73b011b842c5bbd846d79d6a050
Franz Lesly Rocha <franzlesly.rocha.20@usjr.edu.ph>
2022-07-09 21:00:24 +0800

    fetched item from api

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import '../models/api_response.dart';
+import '../models/note.dart';
 import '../models/notes_for_listing.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,15 +17,7 @@ class NotesService {
         final jsonData = json.decode(data.body);
         final notes = <NoteForListing>[];
         for (var item in jsonData) {
-          final note = NoteForListing(
-            noteID: item['noteID'],
-            noteTitle: item['noteTitle'],
-            createDateTime: DateTime.parse(item['createDateTime']),
-            latestEditDateTime: item['latestEditDateTime'] != null
-                ? DateTime.parse(item['latestEditDateTime'])
-                : null,
-          );
-          notes.add(note);
+          notes.add(NoteForListing.fromJson(item));
         }
         return APIResponse<List<NoteForListing>>(data: notes);
       }
@@ -32,4 +25,17 @@ class NotesService {
     }).catchError((_) => APIResponse<List<NoteForListing>>(
         error: true, errorMessage: 'An error occured'));
   }
+
+    Future<APIResponse<Note>?> getNote(String noteID) {
+    return http.get(Uri.parse('$api/notes/$noteID'), headers: headers).then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        
+        return APIResponse<Note>(data: Note.fromJson(jsonData));
+      }
+      return APIResponse<Note>(error: true, errorMessage: 'An error occurred');
+    }).catchError((_) =>
+            APIResponse<Note>(error: true, errorMessage: 'An error occurred.'));
+  }
+
 }
