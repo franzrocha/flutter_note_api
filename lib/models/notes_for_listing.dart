5956c1e147ffec233be01829e2f3fd86ba2be7c9
Franz Lesly Rocha <franzlesly.rocha.20@usjr.edu.ph>
2022-07-10 03:58:40 +0800

    generated JSON in dart

--- lib/models/notes_for_listing.dart
+++ lib/models/notes_for_listing.dart
@@ -1,3 +1,9 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'notes_for_listing.g.dart';
+
+@JsonSerializable()
+
 class NoteForListing {
   String? noteID;
   String? noteTitle;
@@ -8,16 +14,7 @@ class NoteForListing {
       {this.noteID,
       this.noteTitle,
       this.createDateTime,
-      this.latestEditDateTime});
+      this.latestEditDateTime});  
 
-  factory NoteForListing.fromJson(Map<String, dynamic> item) {
-    return NoteForListing(
-      noteID: item['noteID'],
-      noteTitle: item['noteTitle'],
-      createDateTime: DateTime.parse(item['createDateTime']),
-      latestEditDateTime: item['latestEditDateTime'] != null
-          ? DateTime.parse(item['latestEditDateTime'])
-          : null,
-    );
-  }
+  factory NoteForListing.fromJson(Map<String, dynamic> item) => _$NoteForListingFromJson(item);
 }
